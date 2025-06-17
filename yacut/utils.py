36743a5e0bcf1820f23891b8c1b8a5ca4cd0676f
kateschka <sova.krs@gmail.com>
2025-06-17 10:19:54 +0300

    Refactor URLMap import in utils.py to avoid circular import issues; add comments for clarity on import strategy.

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,6 +1,5 @@
 from random import choice
 from .constants import ALLOWED_SYMBOLS, DEFAULT_SHORT_URL_LENGTH
-from .models import URLMap
 
 
 def generate_short_id():
@@ -10,6 +9,9 @@ def generate_short_id():
 
 
 def get_unique_short_id():
+    # Импорт модели для избежания циклического импорта
+    from .models import URLMap
+
     short_id = generate_short_id()
     if URLMap.query.filter_by(short=short_id).first():
         return get_unique_short_id()
@@ -17,6 +19,9 @@ def get_unique_short_id():
 
 
 def create_url_map(original_url, custom_id=None):
+    # Импорт модели для избежания циклического импорта
+    from .models import URLMap
+    
     url_map = URLMap()
     url_map.original = original_url
     url_map.short = custom_id if custom_id else get_unique_short_id()
